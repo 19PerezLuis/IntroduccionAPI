4d762a74f603af51d8d045a0850b013e0e7add25
Luis Fernando Perez Rodriguez <luishost131@gmail.com>
2022-05-10 11:52:39 -0600

    Creacion del segundo EndPoint

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
+app.get('/Hola', function(req, res){
     res.send('Hola Mundo!!')
 });
-
+app.get('/Adios', function(req, res){
+    res.send('Adios mundo!!')
+});
 app.listen(3000, function(){
     console.log('Aplicacion ejemplo, escuchando el puerto 3000!!')
 
